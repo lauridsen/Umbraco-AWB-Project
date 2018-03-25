73ec1cd6c4d01353abb5b65cc7c74dcb8b2093b5
Michael Lauridsen <Pjuker@gmail.com>
2018-03-26 00:44:48 +0200

    git init reup

---
+++ AarhusWebDevCoop/Controllers/ContactFormController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Collections.Specialized;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+	public class ContactFormController : SurfaceController
+	{
+		// GET: ContactForm
+		public ActionResult Index()
+		{
+			return PartialView("_ContactForm", new ContactForm());
+		}
+		[HttpPost]
+		public ActionResult HandleFormSubmit(ContactForm model)
+		{
+			if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+			//send mail n' stuff
+			TempData["success"] = true;
+			TempData["Name"] = model.Name;
+
+			MailMessage message = new MailMessage();
+			message.To.Add("INSERT-MAIL-HERE");
+			message.Subject = model.Subject;
+			message.From = new MailAddress(model.Email, model.Name);
+			message.Body = model.Message;
+
+			using (SmtpClient smtp = new SmtpClient())
+			{
+				smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+				smtp.UseDefaultCredentials = false;
+				smtp.EnableSsl = true;
+				smtp.Host = "smtp.gmail.com";
+				smtp.Port = 587;
+				//Insert own credentials
+				smtp.Credentials = new System.Net.NetworkCredential("INSERT-GMAIL-HERE", "INSERT-PASSWORD-HERE");
+				smtp.EnableSsl = true;
+				// send mail
+				smtp.Send(message);
+			}
+
+			IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+			comment.SetValue("messageName", model.Name);
+			comment.SetValue("email", model.Email);
+			comment.SetValue("subject", model.Subject);
+			comment.SetValue("messageContent", model.Message);
+			//save
+			Services.ContentService.Save(comment);
+
+
+			return RedirectToCurrentUmbracoPage();
+		}
+	}
+}
---
+++ AarhusWebDevCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+	public class ContactForm
+	{
+		[Required(ErrorMessage = "Please enter your name")]
+		[StringLength(200, MinimumLength = 2)]
+		public string Name { get; set; }
+
+		[Required]
+		[StringLength(200, MinimumLength = 2)]
+		[EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+		public string Email { get; set; }
+
+		[Required]
+		[StringLength(200, MinimumLength = 2)]
+		public string Subject { get; set; }
+
+		[Required]
+		[Display(Name = "Enter your message")]
+		[StringLength(200, MinimumLength = 2)]
+		public string Message { get; set; }
+	}
+}
